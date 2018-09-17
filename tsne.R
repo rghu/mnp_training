eb3a4952fca796e9b1d5c0f42a4393e2f4f6ed54
mwsill <m.sill@dkfz.de>
2018-09-17 12:49:11 +0200

    add tSNE

---
+++ tsne.R
@@ -0,0 +1,40 @@
+#-----------------------------------------------------------------------------------
+# tSNE analysis
+#                                                                     
+# Martin Sill
+# m.sill@dkfz.de                                                                  
+# 
+# Note, here tSNE is performed on the principal components (PCs) calculated via the singular value decompostion (SVD) of the beta methylation matrix and not
+# as described in the paper via the eigenvalue decomposition covariance variance matrix of the beta matrix.
+# However, results are comparable.
+#
+# 2018-03-14 UTC
+#------------------------------------------------------------------------------------                   
+#options(max.print = 1000)
+#options(stringsAsFactors = FALSE)
+#options(scipen = 999)
+rm(list=ls())
+
+library(Rtsne)
+library(RSpectra)
+
+source("R/RSpectra_pca.R")
+
+message("loading preprocessed data ...",Sys.time())
+load("./results/betas.ba.RData")
+
+# methylation classed
+y <- as.factor(anno$`methylation class:ch1`)
+
+# sd pre filtering to 32k probes
+betas <- betas[,order(-apply(betas,2,sd))[1:32000]]
+
+# calculate first 94 PCs
+pca <- prcomp_svds(betas,k=94)
+
+# calculate tSNE
+res <- Rtsne(pca$x,pca=F,max_iter=2500,theta=0,verbose=T)
+
+# scatterplot tSNE
+plot(res$Y,pch=19,col=y)
+
